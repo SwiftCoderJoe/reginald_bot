a92c2eea2cef3e3321b61b956ca447aac9a2a178
Joe Cardenas <coderjoe05@gmail.com>
2022-08-29 17:40:40 -0400

    Move pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps : [{
       name: "reginaldbot-production",
       script: './reginald_bot',
+      interpreter: "none",
       watch: false,
       time: true,
       env: {
